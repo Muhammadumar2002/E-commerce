913a9bfbe3d66447f5ac4ff893ded69ca02d102c
Muhammadumar2002 <muhammadumaryunusov86@gmail.com>
2022-08-01 15:24:24 +0500

    Add js section img-showcase__thumbnail

--- js/main.js
+++ js/main.js
@@ -1,9 +1,33 @@
+const modifiers = {
+    imgThumbnailActive:'selected'
+}
+
+ // SHOPPING CART MODAL
   const elSiteHeaderCartLink = document.querySelector('.js-site-header-cart-link');
   const elSiteHeaderCartModal = document.querySelector('.site-header__cart-modal');
+
 if (elSiteHeaderCartLink) {
-  elSiteHeaderCartLink.addEventListener('click', function(evt) {
+  elSiteHeaderCartLink.addEventListener('click', function (evt) {
     evt.preventDefault();
 
-    elSiteHeaderCartModal.classList.toggle('.site-header__cart-modal__open');
+    elSiteHeaderCartModal.classList.toggle('site-header__cart-modal__open');
   });
 }
+
+
+// IMAGE SHOWCASE
+const elsImgShowcaseThumbnailButton = document.querySelectorAll('.js-img-showcase-thumbnail-button');
+const elsImgThumbnail = document.querySelectorAll('.img-showcase__thumbnail');
+
+
+  elsImgShowcaseThumbnailButton.forEach(function (elButton) {
+    elButton.addEventListener('click', function () {
+      // Remove active state from all buttons
+      elsImgThumbnail.forEach(function (elImgThumbnail) {
+        elImgThumbnail.classList.remove(modifiers.imgThumbnailActive );
+      });
+
+      // Add active statevto clicked button
+      elButton.parentElement.classList.add(modifiers.imgThumbnailActive);
+    });
+  });
